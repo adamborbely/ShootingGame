90b0f9d376e9443b8a3b79e665efd94d6bf2ff6e
Adam <borbelya90@gmail.com>
2020-04-27 14:44:58 +0200

    shooting

--- game.js
+++ game.js
@@ -1,17 +1,23 @@
-const leftButton = 65;
-const upButton = 87;
-const rightButton = 68;
-const downButton = 83;
-const movement = 5;
+const leftButton = 65; //A
+const upButton = 87;  //W
+const rightButton = 68; // D
+const downButton = 83; // S
+const space = 32; // space
+const movement = 4;
 
 let lastLoopRun = 0;
 
-const redcube = new Object();
-redcube.element = 'redcube'
-redcube.x = 250;
-redcube.y = 460;
-
 const controller = new Object();
+
+function createSprite(element, x, y, w, h){
+    let result = new Object();
+    result.element = element;
+    result.x = x;
+    result.y= y;
+    result.w = w;
+    result.h = h;
+    return result;
+}
 
 function toggleKey(keyCode, isPressed) {
     if(keyCode === leftButton){
@@ -25,6 +31,25 @@ function toggleKey(keyCode, isPressed) {
     }
     if(keyCode === downButton){
         controller.down = isPressed;
+    }
+    if(keyCode === space){
+        controller.space = isPressed;
+    }
+    
+}
+
+function boundsControll(sprite){
+    if(sprite.x <20){
+        sprite.x = 20;
+    }
+    if(sprite.y < 20){
+        sprite.y = 20;
+    }
+    if(sprite.x +sprite.w > 480){
+        sprite.x = 480 - sprite.w;        
+    }
+    if(sprite.y +sprite.h > 480){
+        sprite.y = 480 - sprite.h;        
     }
 }
 
@@ -47,14 +72,25 @@ function handleControls(){
     if(controller.left){
         redcube.x -= movement;
     }
+    if(controller.space){
+        bullet.x = redcube.x + 9;
+        bullet.y = redcube.y - bullet.h;
+    }
+    boundsControll(redcube);
 }
 
-function showSprites(sprite){
-    setPosition(sprite);
+function showSprites(){
+    setPosition(redcube);
+    setPosition(bullet);
+}
+
+function updatePositions(){
+    bullet.y -=12;
 }
 
 function loop(){
     if(new Date().getTime() - lastLoopRun > 40){
+        updatePositions();
         handleControls();
         showSprites(redcube);
 
@@ -70,5 +106,8 @@ document.onkeydown = function(evt){
 document.onkeyup = function(evt){
     toggleKey(evt.keyCode, false);
 }
+
+const redcube = createSprite('redcube', 250, 460, 20, 20);
+const bullet = createSprite('bullet', 0, -120, 2, 50);
 
 loop();
